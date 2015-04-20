dcb74f71ae294b1bb93307c49b25aa245be957cb
Greyson Nevins <greyman1995@gmail.com>
2015-04-20 13:18:51 -0400

    Fixed

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,11 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'home#index'
+  get '/auth/spotify/callback', to: 'users#spotify'
+  get 'home/show', to: 'home#show'
 
+ 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
